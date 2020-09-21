cb0d4940da79d69b8fb8b97f638cacb9a77c6f86
Mahamadou Djeidy Ba <mdjeidyba@atlassian.com>
2020-09-20 23:23:34 -0700

    Initial Project Add

---
+++ src/test/java/djeidy/springframework/djeidypetclinic/DjeidyPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package djeidy.springframework.djeidypetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjeidyPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
